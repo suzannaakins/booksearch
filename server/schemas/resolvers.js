2af2ec0d7ac0e14c1a6fdf557c71cd7675f00a5d
Garlic Fiend <suzannaakins@gmail.com>
2021-01-15 23:04:59 -0800

    implement apollo server and update auth middleware, start resolver functions for users and books

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,80 @@
+const { Book, User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        //query the CURRENT user logged in using token
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books')
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+
+        //get ALL users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+
+        //get all books
+        books: async () => {
+            return Book.find();
+        },
+
+        //get ONE book by id
+        book: async (parent, { _id }) => {
+            return Book.findOne({ _id });
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+
+                const updatedUser =
+                    await User.findByIdAndUpdate(
+                        { _id: context.user._id },
+                        { $push: { books: { bookId, username: context.user.username } } },
+                        { new: true }
+                    );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    }
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+//import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+//import path module to serve up React front-end code in production
 const path = require('path');
+//import typeDefs + resolvers
+const { typeDefs, resolvers } = require('./schemas');
+//import authorization middleware function
+const { authMiddleware } = require('./utils/auth')
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//integrate Apollo server w/ Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,9 +31,15 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+
 });
